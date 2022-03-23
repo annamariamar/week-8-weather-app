636a0c607fad074a25395460bf75fc2b9841ea94
annamariamar <amarciniak@uoc.edu>
2022-03-23 22:56:24 +0100

    Issue degrees solved

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "3fc240078f798a71d42f3a58e52319fb";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
